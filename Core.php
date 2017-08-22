a0bba8a77b489eeda4b341d08f2db87c016dd903
Ikillnukes <ikillnukes1@gmail.com>
2017-08-22 02:45:18 +0200

    Little fix to get-tags

--- Core.php
+++ Core.php
@@ -24,12 +24,15 @@ class HugeCore extends Core
     public static function Load()
     {
         AppLogger::$CurLogger = new AppLogger();
+        $data = mysqli_fetch_assoc(Query::run("SELECT client_id, client_secret FROM hugelauncher_keys WHERE id='1'"));
+        self::$clientID = $data["client_id"];
+        self::$clientSecret = $data["client_secret"];
         self::getAction();
     }
 
     public static function getAction()
     {
-        AppLogger::$CurLogger->SetEventId($_REQUEST['action']);
+        AppLogger::$CurLogger->SetEventId(@$_REQUEST['action']);
         $req = $_SERVER['REQUEST_METHOD'];
         switch ($req) {
             case 'GET':
@@ -40,11 +43,23 @@ class HugeCore extends Core
                         case "get-tags":
                             $owner = @$_GET['owner'];
                             $repo = @$_GET['repo'];
-                            //echo self::GetRequest(self::StrFormat("http://api.github.com/repos/{0}/{1}/git/refs/tags/", $owner, $repo));
-                            echo self::GetRequest("http://google.com/");
+                            $data = self::GetJSONRequest(self::StrFormat("https://api.github.com/repos/{0}/{1}/git/refs/tags/?client_id={2}&client_secret={3}", $owner, $repo, self::$clientID, self::$clientSecret));
+                            if(!$data)
+                                AppLogger::$CurLogger->AddError("github_error");
+                            else
+                                AppLogger::$CurLogger->AddParameter("data", $data);
                             break;
                         case "get-tree":
                             //GET https://api.github.com/repos/Ikillnukes/PCStats/git/trees/master?client_id=$clientId&client_secret=$clientSecret&recursive=1
+                            $owner = @$_GET['owner'];
+                            $repo = @$_GET['repo'];
+                            $sha = @$_GET['sha'];
+                            $recursive = @$_GET['recursive'];
+                            $data = self::GetJSONRequest(self::StrFormat("https://api.github.com/repos/{0}/{1}/git/trees/{2}?client_id={3}&client_secret={4}&recursive={5}", $owner, $repo, $sha, self::$clientID, self::$clientSecret, $recursive));
+                            if(!$data)
+                                AppLogger::$CurLogger->AddError("github_error");
+                            else
+                                AppLogger::$CurLogger->AddParameter("data", $data);
                             break;
                         default:
                             self::Kill("[GET] Action '".$_GET["action"]."' not set!");
